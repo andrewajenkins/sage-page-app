133fab6cb6a84fb1920000f162f4ed9d7ed2dc77
Andy Jenkins <andy@jenkinssd.com>
2024-02-04 20:23:24 -0800

    split into front and backend projects

---
+++ backend/src/server.ts
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+import { AppDataSource } from "./data-source"
+import { User } from "./entity/User"
+
+AppDataSource.initialize().then(async () => {
+
+    console.log("Inserting a new user into the database...")
+    const user = new User()
+    user.firstName = "Timber"
+    user.lastName = "Saw"
+    user.age = 25
+    await AppDataSource.manager.save(user)
+    console.log("Saved a new user with id: " + user.id)
+
+    console.log("Loading users from the database...")
+    const users = await AppDataSource.manager.find(User)
+    console.log("Loaded users: ", users)
+
+    console.log("Here you can setup and run express / fastify / any other framework.")
+
+}).catch(error => console.log(error))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
