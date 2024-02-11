a19dea5cb5f6d6afbb0f69f78d9fc70b70150af5
Andy Jenkins <andy@jenkinssd.com>
2024-02-11 06:15:09 -0800

    working prod build and deploy - still testing the email submission to db

--- tools/bin/docker-build-prod.sh
+++ tools/bin/docker-build-prod.sh
@@ -1,3 +1,8 @@
+### create new repository
+#aws ecr create-repository --repository-name nginx --region us-east-1
+
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 437884575683.dkr.ecr.us-east-1.amazonaws.com
+
 # For backend
 docker build --no-cache -t backend ./backend
 docker tag backend:latest 437884575683.dkr.ecr.us-east-1.amazonaws.com/backend:latest
@@ -7,3 +12,9 @@ docker push 437884575683.dkr.ecr.us-east-1.amazonaws.com/backend:latest
 docker build --no-cache -t frontend ./frontend
 docker tag frontend:latest 437884575683.dkr.ecr.us-east-1.amazonaws.com/frontend:latest
 docker push 437884575683.dkr.ecr.us-east-1.amazonaws.com/frontend:latest
+
+# For nginx
+docker pull nginx:latest
+docker build --no-cache -t nginx ./tools/nginx
+docker tag nginx:latest 437884575683.dkr.ecr.us-east-1.amazonaws.com/nginx:latest
+docker push 437884575683.dkr.ecr.us-east-1.amazonaws.com/nginx:latest
